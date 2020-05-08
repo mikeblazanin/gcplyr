183153a3207571971f970e71c2a339a877d3165d
mikeblazanin <mikeblazanin@gmail.com>
2020-05-07 22:21:12 -0400

    Testing for make_tidydesign, import_blockcurves

--- testing_functions.R
+++ testing_functions.R
@@ -20,7 +20,8 @@ for (i in 1:100) {
 my_blockcurves1 <- read_blockcurves(
   files = paste("./test_blockcurves_data_csv/",
                 list.files("./test_blockcurves_data_csv/"), sep = ""),
-  startrow = 2, startcol = 2, endrow = 9, endcol = 13)
+  startrow = 2, startcol = 2, endrow = 9, endcol = 13,
+  metadata = list("type1" = c(9, 9), "type2" = c(5, 8)))
 my_blockcurves2 <- read_blockcurves(
   files = paste("./test_blockcurves_data_xlsx/",
                 list.files("./test_blockcurves_data_xlsx/"), sep = ""),
@@ -48,6 +49,16 @@ wide_3 <- import_blockcurves(paste("./test_blockcurves_data_xlsx/",
                              num_plates = 10,
                              infer_colnames = FALSE, infer_rownames = FALSE)
 
+home <- getwd()
+setwd("C:/Users/mikeb/Google Drive/Research Projects/12 Growth Curves/1_2019-07-03_MOIDensityGrowthCurve")
+wide_4 <- import_blockcurves(files = list.files(), extension = "xlsx",
+                             startrow = 25, startcol = 2, endrow = 32, endcol = 13,
+                             infer_colnames = FALSE, infer_rownames = FALSE,
+                             metadata = list("starttime" = c(21, 2),
+                                             "endtime" = c(36, 2),
+                                             "temp" = c(23, 2)))
+setwd(home)
+
 for (i in 1:length(wide_3)) {
   write.csv(wide_3[[i]], paste("./test_widecurve_csv/",
                                formatC(i, width = 2, flag = "0"),
@@ -63,4 +74,22 @@ for (i in 1:length(wide_3)) {
   
 import_widecurves(paste("./test_widecurve_csv/",
                         list.files("./test_widecurve_csv/"), 
-                        sep = "")
+                        sep = ""))
+
+
+setwd("C:/users/mikeb/Documents/code/growth-curves/test_blockdesign")
+des <- read_blockmeasures(list.files())
+setwd(home)
+
+
+tidydes <- make_tidydesign(nrows = 8, ncols = 12,
+                           block_row_names = LETTERS[1:8],
+                           block_col_names = 1:12,
+                Treatment = list(values = c("L", "G", "C"),
+                                 2:7, 2:11, "122333"))
+
+
+
+#Testing make_layout
+dot_args <- list("Isolate" = list(c("A", "B"), 2:7, 2:11, 
+                                  pattern = "1,1,2,2,0,0", TRUE))

43873c9e6d4c4be3702168652f684f9871a24ccb
mikeblazanin <mikeblazanin@gmail.com>
2021-11-19 15:57:45 -0500

    Wrote a couple simple tests of moving_average

--- growth.curves.pkg/tests/testthat/test_processingfunctions.R
+++ growth.curves.pkg/tests/testthat/test_processingfunctions.R
@@ -1,10 +1,26 @@
+context("Processing functions")
+library(testthat)
+library(growth.curves.pkg)
+
 #For testing smooth_data
 
-# set.seed(1)
-# data <- data.frame("time" = 1:100,
-#                    "dens" = 10/(1+exp(-.1*((1:100) - 50))) + rnorm(100, sd = 0.5))
+
 # temp <- mgcv::gam(dens ~ s(time), data=data)
 # temp <- mgcv::gam(formula = formula, data = data)
 # 
 # plot(test$time, test$dens)
 # lines(test$time, temp$fitted)
+
+test_that("Moving average returns correctly", {
+  set.seed(1)
+  data <- data.frame("time" = 1:100,
+                     "dens" = 10/(1+exp(-.1*((1:100) - 50))) + 
+                       rnorm(100, sd = 0.5))
+  
+  expect_error(moving_average(formula = dens ~ time + treat,
+                              data = data,
+                              window_width = 3))
+  expect_error(moving_average(formula = ~ time,
+                              data = data,
+                              window_width = 3))
+})

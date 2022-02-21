c90e36c23ae22187485cafa2c7b5630cc24fe232
mikeblazanin <mikeblazanin@gmail.com>
2022-02-21 01:47:47 -0500

    wrote tests for auc

---
+++ tests/testthat/test_analysisfunctions.R
@@ -0,0 +1,19 @@
+context("Analysis functions")
+library(testthat)
+library(gcplyr)
+
+test_that("auc returns correctly with no xlim", {
+  expect_equal(auc(x = 1:10, y = (1:10)**2), 669/2)
+  expect_equal(auc(x = c(1:5, NA, 6:10), y = c((1:5)**2, NA, (6:10)**2)), 669/2)
+  expect_equal(auc(x = c(1:3, NA, 4:10), y = c((1:7)**2, NA, (8:10)**2)), 375)
+})
+
+test_that("auc returns correctly with xlim", {
+  expect_equal(auc(x = 1:10, y = (1:10)**2, xlim = c(2, 9)), 241.5)
+  expect_equal(auc(x = 1:10, y = (1:10)**2, xlim = c(1, 9)), 244)
+  expect_equal(auc(x = 1:10, y = (1:10)**2, xlim = c(4, 10)), 313)
+  expect_equal(auc(x = 1:10, y = (1:10)**2, xlim = c(3.5, 7.5)), 127)
+  expect_equal(auc(x = 1:10, y = (1:10)**2, xlim = c(2, NA)), 332)
+  expect_equal(auc(x = 1:10, y = (1:10)**2, xlim = c(NA, 7)), 115)
+  expect_error(auc(x = 1:10, y = (1:10)**2, xlim = c(NA, NA)))
+})

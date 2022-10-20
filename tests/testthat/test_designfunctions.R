6c4aa01712ec57872429239d40f2683315205f04
mikeblazanin <mikeblazanin@gmail.com>
2022-10-20 15:16:19 -0400

    updated tests for make_design

--- tests/testthat/test_designfunctions.R
+++ tests/testthat/test_designfunctions.R
@@ -4,7 +4,7 @@ library(gcplyr)
 
 test_that("make_tidydesign issues errors when expected for invalid pattern strings", {
   expect_error(
-    make_tidydesign(nrows = 8, ncols = 12, lookup_tbl_start = "A",
+    make_design(nrows = 8, ncols = 12, lookup_tbl_start = "A",
                     "design_elem" = list(c("test1", "test2", "test3"),
                                          2:4,
                                          5:7,
@@ -13,7 +13,7 @@ test_that("make_tidydesign issues errors when expected for invalid pattern strin
              have lookup_tbl_start correct and that you're only using 
              alphanumeric values")
   expect_error(
-    make_tidydesign(nrows = 8, ncols = 12, pattern_split = ",",
+    make_design(nrows = 8, ncols = 12, pattern_split = ",",
                     "design_elem" = list(c("test1", "test2", "test3"),
                                          2:4,
                                          5:7,
@@ -23,70 +23,74 @@ test_that("make_tidydesign issues errors when expected for invalid pattern strin
 
 test_that("make_tidydesign works as expected for numerical pattern strings", {
   expect_equal(
-    make_tidydesign(nrows = 4, ncols = 4, colnames_first = TRUE,
+    make_design(wellnames_sep = "",
+                nrows = 4, ncols = 4, colnames_first = TRUE,
                     "name" = list(c(1, "a", "hello"),
                                   2:3, 2:4,
                                   pattern = "122333")),
     expected = data.frame(
-      Well = paste(rep(1:4, 4), rep(LETTERS[1:4], each = 4),
-                   sep = ""),
+      Well = paste(rep(1:4, each = 4),
+                   rep(LETTERS[1:4], 4), sep = ""),
       name = c(NA, NA, NA, NA,
-               NA, "1", "a", "a",
-               NA, "hello", "hello", "hello",
-               NA, NA, NA, NA))
+               NA, "1", "hello", NA,
+               NA, "a", "hello", NA,
+               NA, "a", "hello", NA))
   )
 })
 
 test_that("make_tidydesign works with 0's in numerical pattern string", {
   expect_equal(
-    make_tidydesign(nrows = 4, ncols = 4, colnames_first = TRUE,
+    make_design(wellnames_sep = "",
+                nrows = 4, ncols = 4, colnames_first = TRUE,
                   "name" = list(c(1, "a", "hello"),
                                 2:3, 2:4,
                                 pattern = "122033")),
     expected = data.frame(
-      Well = paste(rep(1:4, 4), rep(LETTERS[1:4], each = 4),
-                   sep = ""),
+      Well = paste(rep(1:4, each = 4),
+                   rep(LETTERS[1:4], 4), sep = ""),
       name = c(NA, NA, NA, NA,
-               NA, "1", "a", "a",
-               NA, NA, "hello", "hello",
-               NA, NA, NA, NA))
+               NA, "1", NA, NA,
+               NA, "a", "hello", NA,
+               NA, "a", "hello", NA))
     )
 })
 
 test_that("make_tidydesign works for multiple design elements", {
   expect_equal(
-    make_tidydesign(nrows = 4, ncols = 4, colnames_first = TRUE,
+    make_design(wellnames_sep = "", nrows = 4, ncols = 4, colnames_first = TRUE,
                     "name" = list(c(1, "a", "hello"), 
                                   2:3, 2:4,
                                   pattern = "122033"),
                     "name2" = list(c(1, "a", "hello"),
                                   2:3, 2:4,
                                   pattern = "233011")),
-    expected = data.frame(Well = paste(rep(1:4, 4), rep(LETTERS[1:4], each = 4),
-                                       sep = ""),
-                          name = c(NA, NA, NA, NA, 
-                                   NA, "1", "a", "a",
-                                   NA, NA, "hello", "hello",
-                                   NA, NA, NA, NA),
+    expected = data.frame(Well = paste(rep(1:4, each = 4),
+                                       rep(LETTERS[1:4], 4), sep = ""),
+                          name = c(NA, NA, NA, NA,
+                                   NA, "1", NA, NA,
+                                   NA, "a", "hello", NA,
+                                   NA, "a", "hello", NA),
                           name2 = c(NA, NA, NA, NA, 
-                                    NA, "a", "hello", "hello",
-                                   NA, NA, "1", "1",
-                                   NA, NA, NA, NA))
+                                    NA, "a", NA, NA,
+                                    NA, "hello", 1, NA,
+                                    NA, "hello", 1, NA))
   )
 })
 
 test_that("make_tidydesign works as expected for alpha pattern strings", {
   expect_equal(
-    make_tidydesign(nrows = 4, ncols = 4, colnames_first = TRUE,
+    make_design(wellnames_sep = "",
+                nrows = 4, ncols = 4, colnames_first = TRUE,
                     lookup_tbl_start = "a",
                     "name" = list(c(1, "a", "hello"),
                                   2:3, 2:4,
                                   pattern = "abbccc")),
-    expected = data.frame(Well = paste(rep(1:4, 4), rep(LETTERS[1:4], each = 4),
-                                       sep = ""),
-                          name = c(NA, NA, NA, NA,
-                                   NA, "1", "a", "a",
-                                   NA, "hello", "hello", "hello",
-                                   NA, NA, NA, NA))
+    expected = data.frame(
+      Well = paste(rep(1:4, each = 4),
+                   rep(LETTERS[1:4], 4), sep = ""),
+      name = c(NA, NA, NA, NA,
+               NA, "1", "hello", NA,
+               NA, "a", "hello", NA,
+               NA, "a", "hello", NA))
   )
 })

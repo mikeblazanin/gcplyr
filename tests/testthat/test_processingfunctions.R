20a6bd1b601ef02f40712a53dbb1994300d6de2a
mikeblazanin <mikeblazanin@gmail.com>
2022-11-05 13:37:59 -0400

    tests for gam and loess in smooth_data

--- tests/testthat/test_processingfunctions.R
+++ tests/testthat/test_processingfunctions.R
@@ -111,3 +111,66 @@ test_that("smooth_data returns properly for moving-average", {
                            window_width = 5),
                expected = c(manual_expect_win5, manual_expect_win5 + 10))
 })
+
+test_that("smooth_data returns properly for loess", {
+  set.seed(1)
+  data <- data.frame("time" = 1:100,
+                     "dens" = 10/(1+exp(-.1*((1:100) - 50))) +
+                       rnorm(100, sd = 0.5))
+  expect_equal(
+    smooth_data(x = data$time, y = data$dens,
+                method = "loess", span = 0.5),
+    expected = loess(dens ~ time, data, span = 0.5, 
+                     na.action = "na.exclude")$fitted)
+  
+  #Now test with out-of-order data
+  data2 <- data.frame(x = c(50:100, 1:49), y = sqrt(c(50:100, 1:49)))
+  expect_equal(smooth_data(x = data2$x, y = data2$y,
+                           method = "loess", span = 0.5),
+               expected = loess(y ~ x, data2, span = 0.5,
+                                na.action = "na.exclude")$fitted)
+  
+  #Now test with NA's and out-of-order data
+  data3 <- data.frame(x = c(50:65, NA, 66:100, NA, 2:48, NA), 
+                      y = c(NA, sqrt(50:75), NA, sqrt(76:100), sqrt(1:48)))
+  expected3 <- predict(loess(y ~ x, data3, span = 0.5, na.action = "na.exclude"),
+                       data3)
+  names(expected3) <- NULL
+  expect_equal(smooth_data(x = data3$x, y = data3$y,
+                           method = "loess", span = 0.5),
+               expected = expected3)
+})
+
+test_that("smooth_data returns properly for gam", {
+  library(mgcv)
+  set.seed(1)
+  data <- data.frame("time" = 1:100,
+                     "dens" = 10/(1+exp(-.1*((1:100) - 50))) +
+                       rnorm(100, sd = 0.5))
+  expect1 <- predict(gam(formula = dens ~ s(time), data = data),
+                     data)
+  names(expect1) <- NULL
+  dim(expect1) <- NULL
+  expect_equal(
+    smooth_data(x = data$time, y = data$dens, method = "gam"),
+    expected = expect1)
+
+  #Now test with out-of-order data
+  data2 <- data.frame(x = c(50:100, 1:49), y = sqrt(c(50:100, 1:49)))
+  expect2 <- predict(gam(formula = y ~ s(x), data = data2),
+                     data2)
+  names(expect2) <- NULL
+  dim(expect2) <- NULL
+  expect_equal(smooth_data(x = data2$x, y = data2$y, method = "gam"),
+               expected = expect2)
+  
+  #Now test with NA's and out-of-order data
+  data3 <- data.frame(x = c(50:65, NA, 66:100, NA, 2:48, NA), 
+                      y = c(NA, sqrt(50:75), NA, sqrt(76:100), sqrt(1:48)))
+  expect3 <- predict(gam(formula = y ~ s(x), data = data3),
+                     data3)
+  names(expect3) <- NULL
+  dim(expect3) <- NULL
+  expect_equal(smooth_data(x = data3$x, y = data3$y, method = "gam"),
+               expected = expect3)
+})

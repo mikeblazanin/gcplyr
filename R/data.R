16cf4352ce948c5e5e950d2f52fe9d32ded2b9b9
mikeblazanin <mikeblazanin@gmail.com>
2022-11-23 18:43:46 -0500

    Updated data documentation

--- R/data.R
+++ R/data.R
@@ -2,8 +2,17 @@
 #' 
 #' A dataset containing example growth of 96 wells of simulated bacteria
 #'  or bacteria and phages
+#'  
+#' Wells A1...A8 through F1...F8 contain 48 different
+#' simulated bacterial strains growing alone. Wells G1...G8 through L1...L8
+#' contain the same 48 bacterial strains in an identical layout, but this
+#' time growing in the presence of a phage
+#' 
+#' Bacterial populations exhibit diauxic growth as they approach their
+#' carrying capacity, and they also evolve resistance in the face of 
+#' selection from the phage population.
 #'
-#' @format A dataframe with ___ rows and 97 variables:
+#' @format A dataframe with 97 rows and 97 variables:
 #' \describe{
 #'      \item{time}{time, in seconds, since growth curve began}
 #'      \item{A1, A2...H11, H12}{bacterial density in the given well}

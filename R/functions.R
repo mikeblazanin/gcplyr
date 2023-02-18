81f20fedbe75bba142db84d8a9ef87cbde23bbaf
Mike Blazanin <mikeblazanin@gmail.com>
2023-02-18 10:48:30 -0500

    added lag_time warning

--- R/functions.R
+++ R/functions.R
@@ -4034,6 +4034,11 @@ lag_time <- function(x = NULL, y = NULL, deriv = NULL,
     if(is.null(x) | is.null(deriv)) {stop("x1, or deriv and x, must be provided")}
     x1 <- x[which.max(deriv)]}
   
+  if(var(c(length(y0), length(y1), length(slope), length(x1))) != 0) {
+    warning("Only using the first value")
+    y0 <- y0[1]; y1 <- y1[1]; slope <- slope[1]; x1 <- x1[1]
+  }
+  
   if(!trans_y %in% c("linear", "log")) {
     stop("trans_y must be one of c('linear', 'log')")}
   if(trans_y == "log") {y0 <- log(y0); y1 <- log(y1)}

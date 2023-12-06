1c4e1d3111ee59e19a1d6776046f9683e5be7316
Mike Blazanin <mikeblazanin@gmail.com>
2023-12-06 17:57:00 -0500

    renamed checkdim_inputs in test file

--- tests/testthat/test_utilityfunctions.R
+++ tests/testthat/test_utilityfunctions.R
@@ -1,15 +1,15 @@
 library(testthat)
 library(gcplyr)
 
-test_that("checkdim_inputs returns correct length", {
-  expect_equal(checkdim_inputs("A", "test_name", needed_len = 5), 
+test_that("check_input_dimensions returns correct length", {
+  expect_equal(check_input_dimensions("A", "test_name", needed_len = 5), 
                rep("A", 5))
-  expect_equal(checkdim_inputs(rep("A", 5), "test_name", needed_len = 5), 
+  expect_equal(check_input_dimensions(rep("A", 5), "test_name", needed_len = 5), 
                rep("A", 5))
 })
 
-test_that("checkdim_inputs returns error when appropriate", {
-  expect_error(checkdim_inputs(rep("A", 2), "test_name", needed_len = 5))
+test_that("check_input_dimensions returns error when appropriate", {
+  expect_error(check_input_dimensions(rep("A", 2), "test_name", needed_len = 5))
 })
 
 test_that("uninterleave returns correct output", {

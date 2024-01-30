149b00ce6ed1cfd968acce952a8e7421792fbdd4
mikeblazanin <mikeblazanin@gmail.com>
2024-01-29 20:55:52 -0500

    Added example_design_tidy dataset

--- R/data.R
+++ R/data.R
@@ -44,3 +44,21 @@
 #'      \item{A1, A2...H11, H12}{bacterial density in the given well}
 #'  }
 "example_widedata_noiseless"
+
+#' Design for example growth curve data
+#'  
+#' A tidy-shaped dataset with the experimental design (i.e. plate layout)
+#' for the example data included with \code{gcplyr}.
+#' 
+#' Wells A1...A8 through F1...F8 contain 48 different
+#' simulated bacterial strains growing alone. Wells G1...G8 through L1...L8
+#' contain the same 48 bacterial strains in an identical layout, but this
+#' time growing in the presence of a phage
+#'
+#' @format A dataframe with 96 rows and 3 variables:
+#' \describe{
+#'      \item{Well}{The well of the plate}
+#'      \item{Bacteria_strain}{The numbered bacterial strain growing in each well}
+#'      \item{Phage}{Whether or not the bacteria were simulated growing with phages}
+#'  }
+"example_design_tidy"
---
+++ data-raw/example_design.R
@@ -0,0 +1,17 @@
+library(gcplyr)
+
+example_design_tidy <- make_design(
+  pattern_split = ",", nrows = 8, ncols = 12,
+  "Bacteria_strain" = make_designpattern(
+    values = paste("Strain", 1:48),
+    rows = 1:8, cols = 1:6, pattern = 1:48, byrow = TRUE),
+  "Bacteria_strain" = make_designpattern(
+    values = paste("Strain", 1:48),
+    rows = 1:8, cols = 7:12, pattern = 1:48, byrow = TRUE),
+  "Phage" = make_designpattern(
+    values = c("No Phage"), rows = 1:8, cols = 1:6, pattern = "1"),
+  "Phage" = make_designpattern(
+    values = c("Phage Added"), rows = 1:8, cols = 7:12, pattern = "1"))
+
+#Save
+usethis::use_data(example_design_tidy, overwrite = TRUE)

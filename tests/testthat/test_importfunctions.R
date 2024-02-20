8cf0a81cdfeded397f36636dd1007679b03bd72d
Mike Blazanin <mikeblazanin@gmail.com>
2024-02-19 20:52:39 -0500

    wrote tests for import_blockdesigns, all of which are currently passing

--- tests/testthat/test_importfunctions.R
+++ tests/testthat/test_importfunctions.R
@@ -1,2 +1,72 @@
 library(testthat)
 library(gcplyr)
+
+test_that("import_blockdesigns works correctly", {
+  setwd(tempdir())
+  
+  #Single file, single design
+  dat1 <- matrix(paste0("str", 1:8), nrow = 8, ncol = 12)
+  colnames(dat1) <- 1:12
+  row.names(dat1) <- LETTERS[1:8]
+  write.csv(dat1, "strain.csv")
+  expect_equal(
+    import_blockdesigns("strain.csv"),
+    data.frame(Well = paste0(rep(LETTERS[1:8], each = 12), 1:12),
+               strain = rep(paste0("str", 1:8), each = 12)))
+  
+  #Single file, pasted designs
+  dat2 <- matrix(paste0("str", 1:8), nrow = 8, ncol = 12)
+  dat2 <- matrix(paste0(dat2, "_", LETTERS[1:4]), nrow = 8, ncol = 12)
+  colnames(dat2) <- 1:12
+  row.names(dat2) <- LETTERS[1:8]
+  write.csv(dat2, "strain_rep.csv")
+  expect_equal(
+    import_blockdesigns("strain_rep.csv"),
+    data.frame(Well = paste0(rep(LETTERS[1:8], each = 12), 1:12),
+               strain_rep = rep(paste0("str", 1:8, "_", LETTERS[1:4]), each = 12)))
+  expect_equal(
+    import_blockdesigns("strain_rep.csv", sep = "_"),
+    data.frame(Well = paste0(rep(LETTERS[1:8], each = 12), 1:12),
+               strain = rep(paste0("str", 1:8), each = 12),
+               rep = rep(LETTERS[1:4], each = 12)))
+  
+  #Multiple files, ea w/ single design
+  dat3 <- matrix(paste0("str", 1:8), nrow = 8, ncol = 12)
+  dat4 <- matrix(LETTERS[1:4], nrow = 8, ncol = 12)
+  colnames(dat3) <- 1:12
+  row.names(dat3) <- LETTERS[1:8]
+  colnames(dat4) <- 1:12
+  row.names(dat4) <- LETTERS[1:8]
+  write.csv(dat3, "strain.csv")
+  write.csv(dat4, "rep.csv")
+  expect_equal(
+    import_blockdesigns(c("strain.csv", "rep.csv")),
+    data.frame(Well = paste0(rep(LETTERS[1:8], each = 12), 1:12),
+               strain = rep(paste0("str", 1:8), each = 12),
+               rep = rep(LETTERS[1:4], each = 12)))
+  
+  #Multiple files, ea w/ pasted designs
+  dat5 <- matrix(paste0("str", 1:8), nrow = 8, ncol = 12)
+  dat5 <- matrix(paste0(dat5, "_", LETTERS[1:4]), nrow = 8, ncol = 12)
+  colnames(dat5) <- 1:12
+  row.names(dat5) <- LETTERS[1:8]
+  write.csv(dat5, "strain_rep.csv")
+  dat6 <- matrix(paste0("bact", 1:8), nrow = 8, ncol = 12)
+  dat6 <- matrix(paste0(dat6, "_", LETTERS[5:8]), nrow = 8, ncol = 12)
+  colnames(dat6) <- 1:12
+  row.names(dat6) <- LETTERS[1:8]
+  write.csv(dat6, "bact_rep2.csv")
+  
+  expect_equal(
+    import_blockdesigns(c("strain_rep.csv", "bact_rep2.csv")),
+    data.frame(Well = paste0(rep(LETTERS[1:8], each = 12), 1:12),
+               strain_rep = rep(paste0("str", 1:8, "_", LETTERS[1:4]), each = 12),
+               bact_rep2 = rep(paste0("bact", 1:8, "_", LETTERS[5:8]), each = 12)))
+  expect_equal(
+    import_blockdesigns(c("strain_rep.csv", "bact_rep2.csv"), sep = "_"),
+    data.frame(Well = paste0(rep(LETTERS[1:8], each = 12), 1:12),
+               strain = rep(paste0("str", 1:8), each = 12),
+               rep = rep(LETTERS[1:4], each = 12),
+               bact = rep(paste0("bact", 1:8), each = 12),
+               rep2 = rep(LETTERS[5:8], each = 12)))
+})

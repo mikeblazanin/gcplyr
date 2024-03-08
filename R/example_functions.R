726786397b27167a337492c6d95f90cb5065f196
Mike Blazanin <mikeblazanin@gmail.com>
2024-03-08 13:33:27 -0500

    Added pasted files to design vignette (#194)

--- R/example_functions.R
+++ R/example_functions.R
@@ -156,6 +156,19 @@ make_example <- function(vignette, example, dir = ".") {
                    byrow = TRUE))
       message("Files have been written")
       return(paste0(dir, "mydesign2.csv"))
+    } else if (example == 3) {
+      ## Example 3 ----
+      pt1 <- matrix(rep(c("Tr1", "Tr2"), each = 48),
+                    nrow = 8, ncol = 12, dimnames = list(LETTERS[1:8], 1:12))
+      pt2 <- matrix(rep(c("StrA", "StrB", "StrC", "StrD"), each = 24),
+                    nrow = 8, ncol = 12, dimnames = list(LETTERS[1:8], 1:12),
+                    byrow = TRUE)
+      utils::write.csv(
+        file = paste0(dir, "mydesign_pasted.csv"),
+        x = matrix(paste(pt1, pt2, sep = "_"),
+                   nrow = 8, ncol = 12, dimnames = list(LETTERS[1:8], 1:12)))
+      message("Files have been written")
+      return(paste0(dir, "mydesign_pasted.csv"))
     }
   
   } else if (vignette == 4) {

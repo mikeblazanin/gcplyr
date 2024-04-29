a21073d1789951bccea2626663c60ca9f23de233
Mike Blazanin <mikeblazanin@gmail.com>
2024-04-29 14:36:16 -0400

    Further manuscript plot tweaks

--- manuscript/manuscript_figs.R
+++ manuscript/manuscript_figs.R
@@ -42,115 +42,72 @@ dat_sum <-
             auc = auc(x = Time, y = OD600_norm))
 
 
-#Max density, AUC, max percap, lag time, max percap ----
+#Max density, AUC, lag time, max percap ----
 p1 <- ggplot(dat, aes(x = Time, y = OD600_norm)) +
   geom_point(size = 0.75) +
   labs(x = "Time (hr)", y = "OD600") +
   scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
+  ylim(NA, 0.63) +
   theme_bw() +
-  geom_hline(yintercept = dat_sum$max_dens, lty = 2, col = "red4") +
-  geom_text(aes(x = 13, y = dat_sum$max_dens + 0.02,
-                label = paste("Maximum density =", dat_sum$max_dens)),
-            col = "red4")
+  geom_hline(data = dat_sum, aes(yintercept = max_dens), lty = 2, col = "red3") +
+  geom_text(data = dat_sum,
+            aes(x = 8.5, y = max_dens + 0.04,
+                label = paste("Max. density =", round(max_dens, 2))),
+            col = "red3")
 
 p2 <- ggplot(dat, aes(x = Time, y = OD600_norm)) +
   geom_point(size = 0.75) +
-  geom_ribbon(aes(ymin = 0, ymax = OD600_norm)) +
+  geom_ribbon(aes(ymin = 0, ymax = OD600_norm), alpha = 0.2, 
+              linewidth = 0, fill = "red3") +
   labs(x = "Time (hr)", y = "OD600") +
   scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
+  ylim(NA, 0.63) +
+  theme_bw() +
+  geom_text(data = dat_sum,
+            aes(x = 11.8, y = 0.1, 
+                label = paste("AUC =", round(auc, 2), "OD hrs")),
+            col = "red3")
+
+p3 <- ggplot(data = dat, aes(x = Time, y = log(OD600_norm))) +
+  geom_point(size = 1.2) +
+  geom_point(data = filter(dat, Time >= dat_sum$max_percap_time1,
+                           Time <= dat_sum$max_percap_time2),
+             color = "blue3") +
+  geom_segment(data = dat_sum,
+               aes(x = lag_time, y = log(init_dens), xend = 6,
+                   yend = log(init_dens) + max_percap*(6 - lag_time)),
+               lty = 2, color = "blue3") +
+  geom_vline(data = dat_sum,
+             aes(xintercept = lag_time), lty = 2, color = "red3") +
+  geom_hline(data = dat_sum,
+             aes(yintercept = log(init_dens)),
+             lty = 2) +
+  theme_bw() +
+  coord_cartesian(xlim = c(0, 9), ylim = c(NA, -0.9)) +
+  scale_x_continuous(breaks = c(0, 6)) +
+  labs(x = "Time (hr)", y = "log(OD600)") +
+  geom_text(data = dat_sum,
+            aes(x = 5.7, y = -1, 
+                label = paste("Lag time =", round(lag_time, 2), "hrs")),
+            col = "red3")
+
+p4 <- ggplot(dat, aes(x = Time, y = deriv_percap)) +
+  geom_point(size = 1.2)  +
+  labs(x = "Time (hr)", y = "Cellular growth rate\n(/hr)") +
+  scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
+  ylim(NA, .95) +
+  geom_hline(aes(yintercept = dat_sum$max_percap),
+             col = "red3", lty = 2) +
+  geom_text(aes(x = 12, y = dat_sum$max_percap + 0.07, 
+                label = paste("Max. growth rate =", round(dat_sum$max_percap, 2))),
+            col = "red3", size = 3.5) +
   theme_bw()
 
-p3 <- ggplot(dat, aes(x = Time, y = deriv_percap)) +
-  geom_line()  +
-  labs(x = "Time (hr)", y = "Per-capita\nDerivative (/hr)") +
-  scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
-  theme_bw() +
-  theme(strip.background = element_blank(),
-        strip.text = element_blank())
-
-png("./manuscript/dens_derivs.png", width = 3, height = 5,
-    units = "in", res = 150)
+png("./manuscript/ex_metrics.png", width = 6, height = 5,
+    units = "in", res = 300)
 cowplot::plot_grid(
-  ncol = 1, rel_heights = c(1.1, 1, 1), align = "v",
-  p1, p2, p3)
-dev.off()
-
-
-# max percap, lag, max dens, diauxie ----
-
-# max percap, lag, max dens
-temp_sum <- filter(dat_sum, Phage == "No Phage", type == "nondiaux")
-temp_dat <- filter(dat, Phage == "No Phage", type == "nondiaux")
-p1 <- ggplot(data = temp_dat,
-       aes(x = Time, y = log(Measurements))) +
-  geom_point() +
-  geom_point(data = filter(temp_dat, Time >= temp_sum$max_percap_time1,
-                           Time <= temp_sum$max_percap_time2),
-             color = "red") +
-  geom_abline(data = temp_sum,
-              color = "red",
-              aes(slope = max_percap,
-                  intercept = log(max_percap_dens) -
-                    max_percap*max_percap_time)) +
-  geom_vline(data = temp_sum,
-             aes(xintercept = lag_time), lty = 2) +
-  # geom_hline(data = temp_sum,
-  #            aes(yintercept = log(init_dens))) +
-  geom_hline(data = temp_sum,
-             aes(yintercept = log(max_dens)), lty = 2) +
-  theme_bw() +
-  scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
-  labs(x = "Time (hr)", y = "log(OD600)")
-
-
-#Diauxie
-temp_sum <- filter(dat_sum, Phage == "No Phage", type == "diauxic")
-temp_dat <- filter(dat, Phage == "No Phage", type == "diauxic")
-p2 <- ggplot(temp_dat, 
-             aes(x = Time, y = Measurements)) +
-  geom_point(size = 0.75) +
-  geom_vline(data = temp_sum,
-             aes(xintercept = diauxie_time), lty = 2) +
-  labs(x = "Time (hr)", y = "OD600") +
-  scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
-  theme_bw()
-
-p3 <- ggplot(temp_dat, 
-             aes(x = Time, y = deriv)) +
-  geom_line() +
-  geom_vline(data = temp_sum,
-             aes(xintercept = diauxie_time), lty = 2) +
-  labs(x = "Time (hr)", y = "Derivative (OD600/hr)") +
-  scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
-  theme_bw() +
-  theme(strip.background = element_blank(),
-        strip.text = element_blank())
-
-png("./manuscript/bact_growth.png", width = 6, height = 4,
-    units = "in", res = 150)
-cowplot::plot_grid(
-  p1, cowplot::plot_grid(p2, p3, ncol = 1, align = "hv"),
-  nrow = 1, labels = "AUTO")
-dev.off()
-
-# first maxima & extinction ----
-temp_dat <- filter(dat, Phage == "Phage Added", type == "nondiaux")
-temp_sum <- filter(dat_sum, Phage == "Phage Added", type == "nondiaux")
-
-png("./manuscript/first_maxima.png", width = 4, height = 3.5,
-    units = "in", res = 150)
-ggplot(temp_dat, 
-       aes(x = Time, y = Measurements)) +
-  geom_point(size = 0.75) +
-  geom_point(data = temp_sum,
-             aes(x = first_maxima_x, y = first_maxima_y), 
-             pch = 4, size = 2, color = "red", stroke = 2, alpha = 0.5) +
-  geom_vline(data = temp_sum,
-             aes(xintercept = extin_time), lty = 2) +
-  scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
-  #facet_grid(~Phage) +
-  labs(x = "Time (hr)", y = "OD600") +
-  theme_bw()
+  nrow = 2, rel_heights = c(1.1, 1, 1), align = "hv", labels = "AUTO",
+  p1, p2, p3, p4)
 dev.off()
 
 

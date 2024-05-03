b0de90d0a1e745e9bc819a8bc59a1ff8a67d4287
Mike Blazanin <mikeblazanin@gmail.com>
2024-05-03 00:04:59 -0400

    Updated code & data for new version of MS figs: two dift growth curves side by side, no lag time anymore

--- manuscript/manuscript_figs.R
+++ manuscript/manuscript_figs.R
@@ -6,20 +6,21 @@ library(dplyr)
 
 #This code was run to filter down all the Blazanin et al Travisano:
 # growth curve data into just the example data for this paper
-# temp <- read.csv("./manuscript/Isolate_growth_curves.csv")
+# temp <- read.csv("./manuscript/Isolate_growth_curves2.csv")
 # temp <- dplyr::filter(temp, Date == "2019-09-10", Proj == "125",
-#                       Pop == "Anc", Rep_Well == "1", Media %in% c("50", "25-50"))
+#                       Media == "25-50", 
+#                       (Treat == "Anc" & Rep_Well == 3) |
+#                       (Pop == "A" & Treat == "C" & Rep_Well == 1))
 # temp$OD600_norm <- temp$OD600 - 0.07895 #subtract blank
-# temp <- dplyr::select(temp, Time_s, OD600_norm)
+# temp <- dplyr::select(temp, Treat, Time_s, OD600_norm)
 # write.csv(temp, "./manuscript/Isolate_growth_curves.csv",
 #           row.names = FALSE)
 
 # Load experimental data ----
 dat <- read.csv("./manuscript/Isolate_growth_curves.csv")
-dat$Well <- "A1"
 dat$Time <- dat$Time_s/3600
 
-dat <- mutate(group_by(dat, Well),
+dat <- mutate(group_by(dat, Treat),
               deriv = calc_deriv(y = OD600_norm, x = Time, 
                                  window_width_n = 5),
               deriv_percap = calc_deriv(y = OD600_norm, x = Time,
@@ -28,86 +29,69 @@ dat <- mutate(group_by(dat, Well),
                                         window_width_n = 5))
 
 dat_sum <- 
-  summarize(group_by(dat, Well),
+  summarize(group_by(dat, Treat),
             max_percap = max(deriv_percap, na.rm = TRUE),
-            max_percap_time = Time[which.max(deriv_percap)],
-            max_percap_dens = OD600_norm[which.max(deriv_percap)],
-            init_dens = first_minima(OD600_norm, return = "y",
-                                     window_width_n = 5),
-            lag_time = lag_time(x = Time, y = OD600_norm,
-                                deriv = deriv_percap, y0 = init_dens),
-            max_percap_time1 = Time[which.max(deriv_percap)-2],
-            max_percap_time2 = Time[which.max(deriv_percap)+2],
             max_dens = max(OD600_norm, na.rm = TRUE),
             auc = auc(x = Time, y = OD600_norm))
 
 
-#Max density, AUC, lag time, max percap ----
+#Plots ----
+#Max density
 p1 <- ggplot(dat, aes(x = Time, y = OD600_norm)) +
-  geom_point(size = 0.75) +
+  geom_point(size = 0.75, alpha = 0.8) +
   labs(x = "Time (hr)", y = "OD600") +
   scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
+  facet_grid(~ Treat,
+             labeller = labeller(Treat = c("Anc" = "Ancestor", "C" = "Evolved"))) +
   ylim(NA, 0.63) +
   theme_bw() +
-  geom_hline(data = dat_sum, aes(yintercept = max_dens), lty = 2, col = "red3") +
+  geom_hline(data = dat_sum, aes(yintercept = max_dens), lty = 2, col = "#CD0057") +
   geom_text(data = dat_sum,
-            aes(x = 8.5, y = max_dens + 0.04,
+            aes(x = 8.5, y = max_dens + 0.05,
                 label = paste("Max. density =", round(max_dens, 2))),
-            col = "red3")
+            col = "#CD0057") +
+  theme(strip.text = element_text(size = 12),
+        axis.title = element_text(size = 16))
 
+#AUC
 p2 <- ggplot(dat, aes(x = Time, y = OD600_norm)) +
-  geom_point(size = 0.75) +
-  geom_ribbon(aes(ymin = 0, ymax = OD600_norm), alpha = 0.2, 
-              linewidth = 0, fill = "red3") +
+  geom_point(size = 0.75, alpha = 0.8) +
+  geom_ribbon(aes(ymin = 0, ymax = OD600_norm), alpha = 0.15, 
+              linewidth = 0, fill = "#CD0057") +
   labs(x = "Time (hr)", y = "OD600") +
   scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
-  ylim(NA, 0.63) +
+  facet_grid(~ Treat,
+             labeller = labeller(Treat = c("Anc" = "Ancestor", "C" = "Evolved"))) +
   theme_bw() +
   geom_text(data = dat_sum,
-            aes(x = 11.8, y = 0.1, 
+            aes(x = 11.65, y = 0.04, 
                 label = paste("AUC =", round(auc, 2), "OD hrs")),
-            col = "red3")
+            col = "#CD0057", size = 3.7) +
+  theme(strip.text = element_text(size = 12),
+        axis.title = element_text(size = 16))
 
-p3 <- ggplot(data = dat, aes(x = Time, y = log(OD600_norm))) +
-  geom_point(size = 1.2) +
-  geom_point(data = filter(dat, Time >= dat_sum$max_percap_time1,
-                           Time <= dat_sum$max_percap_time2),
-             color = "blue3") +
-  geom_segment(data = dat_sum,
-               aes(x = lag_time, y = log(init_dens), xend = 6,
-                   yend = log(init_dens) + max_percap*(6 - lag_time)),
-               lty = 2, color = "blue3") +
-  geom_vline(data = dat_sum,
-             aes(xintercept = lag_time), lty = 2, color = "red3") +
-  geom_hline(data = dat_sum,
-             aes(yintercept = log(init_dens)),
-             lty = 2) +
-  theme_bw() +
-  coord_cartesian(xlim = c(0, 9), ylim = c(NA, -0.9)) +
-  scale_x_continuous(breaks = c(0, 6)) +
-  labs(x = "Time (hr)", y = "log(OD600)") +
-  geom_text(data = dat_sum,
-            aes(x = 5.7, y = -1, 
-                label = paste("Lag time =", round(lag_time, 2), "hrs")),
-            col = "red3")
-
-p4 <- ggplot(dat, aes(x = Time, y = deriv_percap)) +
-  geom_point(size = 1.2)  +
-  labs(x = "Time (hr)", y = "Cellular growth rate\n(/hr)") +
+#Max growth rate
+p3 <- ggplot(dat, aes(x = Time, y = deriv_percap)) +
+  geom_point(size = 1.2, alpha = 0.8)  +
+  labs(x = "Time (hr)", y = "Cellular growth\nrate (/hr)") +
+  facet_grid(~ Treat,
+             labeller = labeller(Treat = c("Anc" = "Ancestor", "C" = "Evolved"))) +
   scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
-  ylim(NA, .95) +
-  geom_hline(aes(yintercept = dat_sum$max_percap),
-             col = "red3", lty = 2) +
-  geom_text(aes(x = 12, y = dat_sum$max_percap + 0.07, 
-                label = paste("Max. growth rate =", round(dat_sum$max_percap, 2))),
-            col = "red3", size = 3.5) +
-  theme_bw()
+  ylim(NA, .8) +
+  geom_hline(data = dat_sum, aes(yintercept = max_percap),
+             col = "#CD0057", lty = 2) +
+  geom_text(data = dat_sum, aes(x = 10.5, y = max_percap + 0.08, 
+                label = paste("Max. growth rate =", round(max_percap, 2))),
+            col = "#CD0057", size = 4) +
+  theme_bw() +
+  theme(strip.text = element_text(size = 12),
+        axis.title = element_text(size = 16))
 
-png("./manuscript/ex_metrics.png", width = 6, height = 5,
+png("./manuscript/ex_metrics.png", width = 5, height = 7,
     units = "in", res = 300)
 cowplot::plot_grid(
-  nrow = 2, rel_heights = c(1.1, 1, 1), align = "hv", labels = "AUTO",
-  p1, p2, p3, p4)
+  ncol = 1, align = "hv", labels = "AUTO",
+  p1, p2, p3)
 dev.off()
 
 
